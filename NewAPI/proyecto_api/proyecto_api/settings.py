0123148d2442fef968dca51f574e5a313a3b87cf
MLatino7 <144286757+MLatino7@users.noreply.github.com>
2024-04-09 03:44:11 -0500

    5Update settings.py

--- NewAPI/proyecto_api/proyecto_api/settings.py
+++ NewAPI/proyecto_api/proyecto_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ekkmyq+#u1_a*333uh6bl*++dxxav=g+y%&+)+65et$vr-#vgo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [wantsandgets.azurewebsites.net]
+ALLOWED_HOSTS = ['wantsandgets.azurewebsites.net', '127.0.0.1:8000/']
 
 
 # Application definition
